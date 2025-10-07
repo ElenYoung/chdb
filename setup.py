1c3d7053d36c3ded686a464cc52b23772b07f40d
yyl <2921172308@qq.com>
2025-10-08 00:17:00 +0800

    0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quantchdb",          
-    version="0.1.3",            
+    version="0.1.4",            
     author="Young",
     author_email="13515360252@163.com",
     description="A Well-Encapsulated ClickHouse Database APIs Lib",
