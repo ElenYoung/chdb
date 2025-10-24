069357ace5d08fd0a9d91ea09e4b15f711f81290
yyl <2921172308@qq.com>
2025-10-25 01:14:45 +0800

    0.1.9

--- quantchdb/chdb_commands.py
+++ quantchdb/chdb_commands.py
@@ -7,6 +7,12 @@ from .set_logging import get_logger
 from dotenv import load_dotenv
 from typing import Optional, List, Dict, Annotated
 from .utils import *
+from pandas.api.types import (
+    is_integer_dtype, is_float_dtype, is_bool_dtype, 
+    is_datetime64_any_dtype, is_object_dtype, is_categorical_dtype,
+    is_string_dtype
+)
+from datetime import date, time
 
 load_dotenv()
 
@@ -15,17 +21,19 @@ class ClickHouseDatabase:
         self,
         config: Optional[Dict] = None,
         log_file: str = get_project_dir()+'//logs//clickhouse_db.log',
-        terminal = False,
+        terminal_log = False,
         file_log = False,
         auto_time_process = True
     ):
         self.config = config or self._get_config_from_env()
         self.client = None
         self.auto_time_process = auto_time_process
-        self.logger = get_logger(__name__, log_file=log_file, terminal=terminal, file_log=file_log)
+        self.logger = get_logger(__name__, log_file=log_file, terminal_log=terminal_log, file_log=file_log)
 
     def _get_config_from_env(self) -> Dict:
-        """从环境变量获取ClickHouse配置"""
+        """
+        Get configs from .env
+        """
         return {
             "host": os.getenv("DB_HOST", "localhost"),
             "port": int(os.getenv("DB_PORT", 9000)),
@@ -35,7 +43,7 @@ class ClickHouseDatabase:
         }
 
     def connect(self) -> Client:
-        """建立数据库连接"""
+        """build connection"""
         try:
             self.client = Client(
                 host=self.config["host"],
@@ -51,14 +59,16 @@ class ClickHouseDatabase:
             raise ConnectionError("ClickHouse connection failed") from e
 
     def close(self):
-        """关闭连接（ClickHouse连接池自动管理，通常不需要手动关闭）"""
+        """close connection (clickhouse manages connection automatically)"""
         if self.client:
             self.client.disconnect()
             self.logger.debug("ClickHouse connection closed")
 
     @contextmanager
     def cursor(self):
-        """提供游标上下文管理器"""
+        """
+        provide cursor
+        """
         try:
             if not self.client:
                 self.connect()
@@ -67,10 +77,10 @@ class ClickHouseDatabase:
             self.logger.error(f"Query execution failed: {str(e)}")
             raise
         finally:
-            pass  # ClickHouse连接自动管理，无需显式关闭
+            pass  # ClickHouse manages connections automatically
 
     def execute(self, sql: str):
-        """执行SQL语句"""
+        """Execute SQL sentence"""
         with self.cursor() as client:
             try:
                 self.logger.debug(f"Executing SQL: {sql}")
@@ -86,25 +96,25 @@ class ClickHouseDatabase:
             df: pd.DataFrame,
             table_name: str,
             columns: Annotated[list,"the columns you want to insert into table"]=None,
-            datetime_cols: Annotated[list,"the list of columns with datetime type, not date type columns which haven't got tz"]=None,
-            nullable_cols: Annotated[list, "the list of columns whose types are Nullable, not including Nullable Float"]=None
+            datetime_cols: Annotated[list,"the list of columns with datetime type"]=None,
+            convert_tz: Annotated[bool, "True denotes convert the timezone to Asia/Shanghai"]=True
     ):
-        """优化版DataFrame插入"""
+        """
+        Insert DataFrame into database.table.
+        """
         try:
             if self.auto_time_process:
                 if datetime_cols:
                     for datetime_col in datetime_cols:
                         if datetime_col in df.columns:
-                            df[datetime_col] = convert_to_shanghai(pd.to_datetime(df[datetime_col]))
+                            df[datetime_col] = pd.to_datetime(df[datetime_col])
+                            if convert_tz:
+                                df[datetime_col] = convert_to_shanghai(df[datetime_col])
 
-                # if 'date_time' in df.columns:
-                #     # 转换为带时区的时间戳
-                #     df['date_time'] = convert_to_shanghai(pd.to_datetime(df['date_time']))
-
-            if nullable_cols:
-                for nullable_col in nullable_cols:
-                    if nullable_col in df.columns:
-                        df[nullable_col] = convert_to_nullable_object(df[nullable_col])
+            #Convert null type to None, which is acceptable in clickhouse for int/string/bool types
+            na_ser = df.isna().any()
+            na_cols = list(na_ser[na_ser == True].index)
+            df[na_cols] = df[na_cols].apply(convert_to_nullable_object, axis=0)
 
             if columns is None:
                 columns = list(df.columns)
@@ -120,16 +130,129 @@ class ClickHouseDatabase:
         except ClickHouseError as e:
             self.logger.error(f"Insert failed: {e.message}")
             raise
-
+    
     def fetch(self, sql: str, as_df: bool = True) -> pd.DataFrame:
-        """执行查询并返回结果"""
+        """fetch data based on sql sentence"""
         try:
             with self.cursor() as client:
                 result, meta = client.execute(sql, with_column_types=True)
                 if as_df:
                     columns = [col[0] for col in meta]
-                    df = pd.DataFrame(result, columns=columns)
-                return df
+                    res = pd.DataFrame(result, columns=columns)
+                else:
+                    res = (result, meta)
+                return res
         except ClickHouseError as e:
             self.logger.error(f"Query failed: {str(e)}")
             raise
+    
+    def create_table_from_df(self,
+            df: pd.DataFrame,
+            table_name: str,
+            dtypes: Annotated[dict, "the specific dtypes you want to set, such as {'col1':'Int64'}. types are automatically inferred by default"]={},
+            engine: Annotated[str, "the Engine of table, 'MergeTree' by default"]='MergeTree()',
+            orderby: Annotated[str, "ORDER BY sentence of CREATE sentence, 'tuple()' by default"] = 'tuple()',
+            other: Annotated[str, "other suffix sentence you want to add, such as 'PARTITION BY toYear(TradingDate)'"]=None
+        ):
+        """Create table based on DataFrame"""
+        try:
+            columns_with_types = self.infer_clickhouse_schema(df)
+            datetime_cols = []
+            for col, dtype in dtypes.items():
+                columns_with_types[col] = dtype
+                if 'Date' in dtype:
+                    datetime_cols.append(col)
+
+            columns_def = ', '.join([f"{col} {dtype}" for col, dtype in columns_with_types.items()])
+            
+            sql = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns_def}) ENGINE = {engine}"
+            if orderby:
+                sql += f" ORDER BY {orderby}"
+            if other:
+                sql += f" {other}"
+
+            with self.cursor() as cursor:
+                cursor.execute(sql)
+            
+            with self.cursor() as cursor:
+                self.insert_dataframe(df=df, table_name=table_name, datetime_cols=datetime_cols)
+
+            self.logger.info(f"Table {table_name} created successfully")
+        except ClickHouseError as e:
+            self.logger.error(f"Create table failed: {e.message}")
+            raise
+
+
+    def infer_clickhouse_schema(self, df: pd.DataFrame) -> Dict[str, str]:
+        """
+        mapping dtypes of pandas DataFrame to ClickHouse dtypes
+        """
+        schema = {}
+        
+        for col in df.columns:
+            dtype = df[col].dtype
+            col_series = df[col]
+            
+            # check null（NaN/NaT/pd.NA）
+            has_missing = col_series.isna().any()
+            
+            # Int
+            if is_integer_dtype(dtype):
+                min_val = col_series.min()
+                max_val = col_series.max()
+                
+                if min_val >= 0:  
+                    if max_val <= 255:
+                        base_type = "UInt8"
+                    elif max_val <= 65535:
+                        base_type = "UInt16"
+                    elif max_val <= 4294967295:
+                        base_type = "UInt32"
+                    else:
+                        base_type = "UInt64"
+                else:  
+                    if min_val >= -128 and max_val <= 127:
+                        base_type = "Int8"
+                    elif min_val >= -32768 and max_val <= 32767:
+                        base_type = "Int16"
+                    elif min_val >= -2147483648 and max_val <= 2147483647:
+                        base_type = "Int32"
+                    else:
+                        base_type = "Int64"
+                
+                schema[col] = f"Nullable({base_type})" if has_missing else base_type
+            
+            # 2. Float
+            elif is_float_dtype(dtype):
+                if col_series.dtype == 'float32':
+                    base_type = "Float32"
+                else:
+                    base_type = "Float64"
+                schema[col] = f"Nullable({base_type})" if has_missing else base_type
+            
+            # 3. Bool
+            elif is_bool_dtype(dtype):
+                schema[col] = f"Nullable(UInt8)" if has_missing else "UInt8"
+            
+            # 4. DateTime
+            elif is_datetime64_any_dtype(dtype):
+                
+                if all(ts.time() == time(0, 0) for ts in col_series if not pd.isna(ts)):
+                    base_type = "Date"
+                else:
+                    base_type = "DateTime"
+                schema[col] = f"Nullable({base_type})" if has_missing else base_type
+            
+            # 5. String
+            elif is_object_dtype(dtype) or is_string_dtype(dtype) or is_categorical_dtype(dtype):
+                if all(isinstance(x, date) for x in col_series if not pd.isna(x)):
+                    base_type = "Date"
+                    schema[col] = f"Nullable({base_type})" if has_missing else base_type
+                else:
+                    schema[col] = f"Nullable(String)" if has_missing else "String"
+            
+            # 6. Default
+            else:
+                schema[col] = f"Nullable(String)" if has_missing else "String"
+        
+        return schema
--- quantchdb/set_logging.py
+++ quantchdb/set_logging.py
@@ -8,18 +8,19 @@ DEFAULT_LOG_FILE = "default_log.log"
 #默认格式
 DEFAULT_LOG_FORMAT =  "%(asctime)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
 
-def get_logger(name=None, log_file=None, level=logging.DEBUG, terminal=False, file_log=True):
-    """获取一个日志记录器，并支持自定义日志文件和是否在终端输出"""
+def get_logger(name=None, log_file=None, level=logging.DEBUG, terminal_log=False, file_log=True):
+    """
+    Get a logger which support terminal log and file log with switch.
+    
+    """
     logger = logging.getLogger(name)
     if logger.hasHandlers():
-        return logger  # 防止重复添加处理器
+        return logger  
 
     logger.setLevel(level)
 
-    # 创建格式化器
     formatter = logging.Formatter(DEFAULT_LOG_FORMAT)
 
-    # 配置彩色日志的样式
     colored_formatter = coloredlogs.ColoredFormatter(
         fmt=DEFAULT_LOG_FORMAT,
         level_styles={
@@ -35,14 +36,12 @@ def get_logger(name=None, log_file=None, level=logging.DEBUG, terminal=False, fi
         }
     )
 
-    # 只有在 terminal=True 时才添加终端处理器
-    if terminal:
+    if terminal_log:
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(colored_formatter)
         stream_handler.setLevel(level)
         logger.addHandler(stream_handler)
 
-    # 设置文件处理器（始终写入文件）
     if file_log:
         file_path = log_file or DEFAULT_LOG_FILE
         log_dir = os.path.dirname(file_path)
